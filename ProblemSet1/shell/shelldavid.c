3d02a778b018369083d1e00d1d27ec903586d5f2
David <david@ubuntu.(none)>
2013-02-11 14:16:04 +0100

    shelldavid

--- ProblemSet1/shell/shelldavid.c
+++ ProblemSet1/shell/shelldavid.c
@@ -16,129 +16,93 @@
 
 
 int main (int argc,char *argv[]){
-  //pid_t cpid[100];
   char* args[100];
-  //char *out[100];
-  //char in[100];
-  //char *hej[100] ={"parse.c", NULL} ;
   enum cmd_pos pos;
   pid_t pid;
-  int i, p=0, q=0, n;
-  //char temp[100];
-
+  int i, q=0, n;
   char* temp = malloc(100);
   
-  
-
-  //trim(in);
- 
-  //printf("%s\n ", in);
-
 
   while(1){
     
     fprintf(stderr, ">");
-    //fgets(in, 100, stdin);
     n = read(0, temp, 100);
     Errors(n, "bad input");
     temp[n-1] = '\0';
-    //strcpy(temp, in);
+
+
     i = 0;
     int counter = 1;
-     while(temp[i]){
+    while(temp[i]){
       if(temp[i] == '|'){
 	counter++;}
       i++;
     
-     }
-    
-
+    }
     int pipes[counter][2];
     
     for(i = 0; i<counter; i++){
       Errors(pipe(pipes[i]),"Failar att göra pipes");
-  } 
-
+    } 
+     
     i = 0;
 
     do{
-  
- 
-  
-  /*for(i = 0; args[i] != NULL;i++){
-    printf("args[%d]: %s\n",i, args[i]);
-  } 
-   printf("pos: %d\n", pos);
-  */
 
-  pid = fork();
-  pos = next_command(temp, args);
-  switch(pid){
-    
-  case -1: 
-    perror("Forken fuckaur");
-    exit(EXIT_FAILURE);
-  case 0:
-    
-    switch(pos){
-    case unknown:
-      perror("pos = unknown");
-      exit(EXIT_FAILURE);
-    case single:
-      Errors(close(pipes[i][0]),"Close Single");
-      Errors(close(pipes[i][1]),"Close Single");
-      //fprintf(stdout, "SINGLE!");
-      break;
-    case first:
-      //fprintf(stdout, "FIRST!");
-      Errors(dup2(pipes[(i+1)][1], STDOUT_FILENO), "Dup First");
-      //dup2(pipes[i+1][1], STDOUT_FILENO);
-      //close(pipes[i][0]);
-      break;
-    case middle:
-      //fprintf("MIDDLE!");
-      Errors(close(pipes[i][1]), "close middle");      
-      Errors(dup2(pipes[i][0], STDIN_FILENO), "Dup Middle");
-      Errors(dup2(pipes[(i+1)][1], STDOUT_FILENO), "Dup Middle");
-      break;
-    case last:
-      //fprintf("LAST!");
-      Errors(close(pipes[i][1]), "close last");
-      Errors(dup2(pipes[i][0], STDIN_FILENO), "Dup Last");
-      //dup2(STDOUT_FILENO, pipes[i][1]);
-      break;
-      // default:
-      //break;
-    }
-    
-    Errors(q = execvp(args[0],args), "Execvp error"); 
 
-    if(q == -1){
-      perror("execvp funkar inte!");
-      exit(EXIT_FAILURE);
-    }
+      pid = fork();
+      pos = next_command(temp, args);
+      switch(pid){
     
-    //exit(EXIT_SUCCESS);
-  default:
-    // cpid[p] = pid;
-    //OBS PÅ P!
-    //p++;
-    //wait(NULL);
-    close(pipes[i][0]);
-    close(pipes[i][1]);
-    i++;
-  }
+      case -1: 
+	perror("Forken fuckaur");
+	exit(EXIT_FAILURE);
+      case 0:
+    
+	switch(pos){
+	case unknown:
+	  perror("pos = unknown");
+	  exit(EXIT_FAILURE);
+	case single:
+	  Errors(close(pipes[i][0]),"Close Single");
+	  Errors(close(pipes[i][1]),"Close Single");
+	  break;
+	case first:
+	  Errors(dup2(pipes[(i+1)][1], STDOUT_FILENO), "Dup First");
+	  break;
+	case middle:
+      
+	  Errors(close(pipes[i][1]), "close middle");      
+	  Errors(dup2(pipes[i][0], STDIN_FILENO), "Dup Middle");
+	  Errors(dup2(pipes[(i+1)][1], STDOUT_FILENO), "Dup Middle");
+	  break;
+	case last:
+	  Errors(close(pipes[i][1]), "close last");
+	  Errors(dup2(pipes[i][0], STDIN_FILENO), "Dup Last");
+      
+	  break;
+	  default:
+	  break;
+	}
+    
+	Errors(q = execvp(args[0],args), "Execvp error"); 
+
+    
+      default:
+	close(pipes[i][0]);
+	close(pipes[i][1]);
+	i++;
+      }
 
  
     }while(pos != last && pos != single);
     
-    //wait(NULL) på alla i (barnen)
-    
+    //free (temp);
     int x;
     for(x=0;x<i;x++){
       wait(NULL);
     }
   
   }
-return 0;
+  return 0;
 }
