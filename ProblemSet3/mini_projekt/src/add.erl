985ee69d5424a9229ce405ac7a8a655dd9d37d31
Root <ubuntu@ubuntu.(none)>
2013-03-19 06:40:45 -0700

    mapp för miniprojekt

---
+++ ProblemSet3/mini_projekt/src/add.erl
@@ -0,0 +1,23 @@
+%% @doc Erlang mini project.
+-module(add).
+-export([start/3, start/4]).
+
+%% @doc TODO: add documentation
+-spec start(A,B,Base) -> ok when 
+      A::integer(),
+      B::integer(), 
+      Base::integer().
+
+start(A,B, Base) ->
+    tbi.
+
+%% @doc TODO: add documentation
+-spec start(A,B,Base, Options) -> ok when 
+      A::integer(),
+      B::integer(), 
+      Base::integer(),
+      Option::atom() | tuple(),
+      Options::[Option].
+
+start(A,B,Base, Options) ->
+    tbi.
