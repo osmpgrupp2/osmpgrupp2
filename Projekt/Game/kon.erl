22c161843887bb07a57c043a91c798849e879543
Malin <malin.lundberg91@gmail.com>
2013-05-07 08:07:23 -0700

    lite bugg fixar mm

--- Projekt/Game/kon.erl
+++ Projekt/Game/kon.erl
@@ -13,8 +13,9 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([start/0,res/0,rand/0]).
 
+%%-export([start/0,res/0,rand/0]).
+-compile(export_all).
 
 
 %% ====================================================================
@@ -36,21 +37,18 @@ res(Checker, ShotCreator) ->
     receive
 	{left} ->
 	    io:format("left~n",[]),
-	    Checker ! {ship,{X,Y},left},
+	    Checker ! {ship,{x,y},left}, %% {x,y} ska vara den faktiska positionen för skeppet
 	    
-	    res();
+	    res(Checker, ShotCreator);
 	{right} ->
 	    io:format("right~n",[]),
-	    Checker ! {ship,{X,Y},right},
+	    Checker ! {ship,{x,y},right}, %% {x,y} ska vara den faktiska positionen för skeppet
 	    
-	    res();
+	    res(Checker, ShotCreator);
 	{space} ->
 	    io:format("space~n",[]),
 	    ShotCreator ! {new},
-		res()
-	
-
-		
+		res(Checker, ShotCreator)		
     end.
 
 
@@ -62,10 +60,10 @@ start() ->
     io:format("~p~n",[Nod]),
     {boxarn,hoppsansa@ubuntu} ! {self(),1000,1000},
     CheckerStart = spawn_link(kon,checkerStart,[10]),
-    Counter = spawn_link(kon,counter,[]),
+    _Counter = spawn_link(kon,counter,[]),
     ShotCreator = spawn_link(kon,shotCreator,[]),
-    MeteorCreator = spawn_link(kon,meteorCreator,[CheckerStart]),
-    res(Checker, ShotCreator).
+    _MeteorCreator = spawn_link(kon,meteorCreator,[CheckerStart]),
+    res(CheckerStart, ShotCreator).
 
 
 checkerStart(N) ->
@@ -74,42 +72,59 @@ checkerStart(N) ->
     
 
 
-checker(Matrix)
+checker(Matrix) ->
     receive
 	{ship,{X,Y},left} ->
-	    grid:check_elem(X-1,Y,Matrix),
-	    NewMatrix = grid:move_elem_l(3,X,Y,Matrix),
-	    {boxarn,hoppsansa@ubuntu} ! {move,ship,left},
-	    checker(NewMatrix);
+	    {Bool, Type} = grid:check_elem(X-1,Y,Matrix),
+	    if Bool ->
+		    NewMatrix = grid:move_elem_l(3,X,Y,Matrix),
+		    {boxarn,hoppsansa@ubuntu} ! {move,ship,left},
+		    checker(NewMatrix);
+	       true ->
+		    case Type of
+			1 ->
+			    skott;
+			2 ->
+			    meteor
+		    end
+	    end;
 
 	{ship,{X,Y},right} ->
 	    grid:check(X+1,Y,Matrix),
-	    grid:move(X+1,Y,Matrix),
-	    {boxarn,hoppsansa@ubuntu} ! {move,ship,right};
+	    NewMatrix = grid:move_elem_r(3,X,Y,Matrix),
+	    {boxarn,hoppsansa@ubuntu} ! {move,ship,right},
+	    checker(NewMatrix);
+
 	{meteor,{X,Y},MPID} ->
 	    grid:check(X,Y+1,Matrix),
-	    grid:move(X,Y+1,Matrix),
-	    {boxarn,hoppsansa@ubuntu} ! {move,meteor,PID};
+	    NewMatrix = grid:move_elem_down(2,X,Y,Matrix),
+	    {boxarn,hoppsansa@ubuntu} ! {move,meteor,MPID},
+	    checker(NewMatrix);
+
 	{shot,{X,Y},SPID} ->
 	    grid:check(X,Y-1,Matrix),
-	    grid:move(X,Y-1,Matrix),
-	    {boxarn,hoppsansa@ubuntu} ! {move,shot,PID};
+	    NewMatrix = grid:move_elem_up(1,X,Y,Matrix),
+	    {boxarn,hoppsansa@ubuntu} ! {move,shot,SPID},
+	    checker(NewMatrix);
 	
 	{meteor,{X,Y},MPID,1} ->
 	    grid:check(X,Y+1,Matrix),
-	    grid:add(,,),
-	    add to list,
-
-%grid:move(X,Y+1,Matrix),
-	    
-	    {boxarn,hoppsansa@ubuntu} ! {add,meteor,{PID,X}};
+	    NewMatrix = grid:change_elem(2,X,Y,Matrix),
+	    add_to_list,
+      	    {boxarn,hoppsansa@ubuntu} ! {add,meteor,{MPID,X}},
+	    checker(NewMatrix);
 	
 	{shot,{X,Y},SPID,1} ->
 	    grid:check(X,Y-1,Matrix),
-	    grid:move(X,Y-1,Matrix),
-	    {boxarn,hoppsansa@ubuntu} ! {add,shot,{PID,X}};
+	    NewMatrix = grid:change_elem(1, X, Y, Matrix),
+	    add_to_list,
+	    {boxarn,hoppsansa@ubuntu} ! {add,shot,{SPID,X}},
+	    checker(NewMatrix);
+
 	{counter} ->
-	    iterera över listan.
+	    iterera_over_listan,
+	    checker(Matrix)
+	end.
 
 
 meteorCreator(CheckerStart,X) ->
@@ -134,17 +149,17 @@ counter(Checker) ->
 
 spawnshot() ->
     receive
-
+	_ -> ok
 	end.
 
 spawnMeteor() ->
     receive
-
+	_ -> ok
 	end.
     
 
 rand() ->
     Rand = random:seed(2,5,8),
-    {X,Y,Z} = Rand,
+    {X,_Y,_Z} = Rand,
     io:format("~p~n",[X]).
 
